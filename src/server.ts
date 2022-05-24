5f9360234594ffcb1b0876af05b44e10a3577fa3
Lucas Moraes <lucmoraes27@hotmail.com>
2022-05-23 22:13:40 -0300

    Adicionando variavel para porta do servidor

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json())
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log('Servidor rodando na porta 3333!')
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`Servidor rodando na porta ${process.env.PORT || 3333}!`)
 });
